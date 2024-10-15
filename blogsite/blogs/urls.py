e81e96bbb947aed868ebd8c2d8bee8a10fcd42fd
jbatoast <jbamador@up.edu.ph>
2024-10-15 15:08:36 +0800

    Working token authentication system.

--- blogsite/blogs/urls.py
+++ blogsite/blogs/urls.py
@@ -3,8 +3,13 @@ from django.urls import path
 from . import views
 
 # This is where the url patterns for a specific app is stored (in this case the "blogs" app). You only define patterns here after removing the "top level url" i.e. someipaddr.com/blogs/5/results/ (only /5/results/ since someipaddr.com/blogs is "top level url")
+app_name = "blogs" # for namespacing urls in non verbose implementation of url matching in the views.py and templates methods
 urlpatterns = [
     path("", views.index, name = "index"),
     path("<int:blogpost_id>/", views.detail, name = "detail"),
-    path("<int:blogpost_id>/", views.vote, name = "vote"),
+    path("write/", views.create_blogpost, name = "create_blogpost"),
+    # path("accounts/register/", views.register, name = register),
+    path("login", views.login),
+    path("signup", views.signup),
+    path("test_token", views.test_token),
 ]
--- blogsite/blogs/views.py
+++ blogsite/blogs/views.py
@@ -1,39 +1,69 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.template import loader
-
 from .models import BlogPost
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view,authentication_classes, permission_classes
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
+# These functions are called when accessing specific url pages
+# sequence: urls.py -> views.py -> /templates/app/xxxxx.html
 
 # Create your views here.
 def index(request):
-    # return HttpResponse("Hello!, you are in the blogs index.")
-    all_blogs = BlogPost.objects.order_by("-created_at")
-    template = loader.get_template("blogs/index.html")
-    context = {
-        "all_blogs": all_blogs,
-    }
-    # output = "\n".join([bp.title for bp in all_blogs])
-    return HttpResponse(template.render(context, request))
+    # VERBOSE VERSION
+    # # return HttpResponse("Hello!, you are in the blogs index.")
+    # all_blogs = BlogPost.objects.order_by("-created_at")
+    # template = loader.get_template("blogs/index.html")
+    # context = {
+    #     "all_blogs": all_blogs,
+    # }
+    # # output = "\n".join([bp.title for bp in all_blogs])
+    # return HttpResponse(template.render(context, request))
 
-    ''' # NON VERBOSE VERSION - ish:
+    # NON VERBOSE VERSION - ish:
     all_blogs = BlogPost.objects.order_by("-created_at")
     context = {
         "all_blogs": all_blogs,
     }
-    return render(request, "polls/index.html", context)
-    '''
+    return render(request, "blogs/index.html", context)
 
 def detail(request, blogpost_id):
-    
-    ''' NON VERBOSE-ish
-    try:
-        blog = BlogPost.objects.get(pk = blogpost_id)
-    except BlogPost.DoesNotExist:
-        raise Http404("BlogPost does not exist.")
-    return render(request, "blogs/detail.html", {"blogpost": blogpost})
-    '''
     blogpost = get_object_or_404(BlogPost, pk=blogpost_id)
     return render(request, "blogs/detail.html", {"blogpost":blogpost})
 
-def vote(request, blogpost_id):
-    return HttpResponse("You are voting on blogpost %s" % (blogpost_id))
+def create_blogpost(request):
+    return render(request, "blogs/create_blogpost.html")
+
+
+
+@api_view(['POST'])
+def login(request):
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+    token = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
+    return Response({"token": token[0].key, "user": serializer.data})
+
+@api_view(['POST'])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=request.data['username'])
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+        return Response({"token": token.key, "user": serializer.data})
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def test_token(request):
+    """Check if auth token is still valid"""
+    return Response("Passed for %s" % (request.user.email))
